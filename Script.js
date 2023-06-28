264e1115c069a4191ce41ff22fd4838699cbc0a2
ReymondAcosta <mrxd3112@gmail.com>
2023-06-27 21:57:19 -0500

    Initial commit

---
+++ Script.js
@@ -0,0 +1,23 @@
+function showalert() {alert('Te dije que no lo oprimieras desobediente -_-'); }
+const form = document.getElementById('Elform');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  validateForm();
+});
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Ingresa tu correo bien, pinche pendejo');
+    } else {
+      alert('Ahora seras doxeado :)');
+    }
+  }
+  document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
